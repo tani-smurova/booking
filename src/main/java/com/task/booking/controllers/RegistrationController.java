363805cae86eeccb7ca25e876ea6b54344075e3c
tani-smurova <tani-smurova@yandex.ru>
2020-08-19 16:13:47 +0300

    add mail sender and activated code

--- src/main/java/com/task/booking/controllers/RegistrationController.java
+++ src/main/java/com/task/booking/controllers/RegistrationController.java
@@ -1,21 +1,21 @@
 package com.task.booking.controllers;
 
-import com.task.booking.models.Role;
 import com.task.booking.models.User;
-import com.task.booking.repo.UserRepository;
+import com.task.booking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration(){
@@ -24,17 +24,23 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Map <String, Object> model){
-        User userFromDb = userRepository.findByUsername(user.getUsername());
 
-        if (userFromDb !=null){
+        if (!userService.addUser(user)){
             model.put("message", "User exist");
             return "registration";
         }
-
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepository.save(user);
-
         return "redirect:/login";
+    }
+    @GetMapping("/activate/{code}")
+    public String activate (Model model, @PathVariable String code){
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated){
+            model.addAttribute("message", "User successfully activated");
+        } else {
+            model.addAttribute("message", "Activation code is not found");
+        }
+
+        return "login";
     }
 }
